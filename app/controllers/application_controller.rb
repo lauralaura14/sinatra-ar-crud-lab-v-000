efdc419841d33737530a00e15761fbf21ab2809b
Laura <laurao148@gmail.com>
2018-05-18 02:32:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/posts/:id' do
     @post = Post.find(params[:id])
-    @post.update(name: params[:name], content: params[:content])
+    Post.update(name: params[:name], content: params[:content])
     erb :show
   end
 
